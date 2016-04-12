cfec10561430859f06ee4407780abc5cf67ebd88
lyon <326277403@qq.com>
2016-04-12 23:13:27 +0800

    learn vue

--- learn-vue-notebook/learning-vue/04-vue's-event-system/app.js
+++ learn-vue-notebook/learning-vue/04-vue's-event-system/app.js
@@ -1,1 +1,17 @@
-app.js
+new Vue({
+	el: "#demo",
+
+	methods: {
+		touchMe: function() {
+			alert("警察叔叔，这边有变态碰我！");
+		},
+		keyUpEnter: function() {
+			if(this.i_love_you == 'i love you') {
+				alert("i love you too");
+			}
+			else {
+				alert("i don't love you");
+			}
+		}
+	}
+});
---
+++ learn-vue-notebook/learning-vue/05-base/app.js
@@ -0,0 +1,42 @@
+new Vue({
+	el: "#post",
+
+	data: {
+		isVisible: true,
+		likeNumber: 0,
+		unlikeNumber: 0,
+		liked: false,
+		unliked: false,
+	},
+
+	methods: {
+		toggleLike: function () {
+			// 如果再次被选中
+			if(this.liked) {
+				this.likeNumber--;
+			} else {
+				// 如果换选
+				if(this.unliked == true) {
+					this.unliked = false;
+					this.unlikeNumber--;
+				}
+				this.likeNumber++;
+			}
+			this.liked = ! this.liked;
+		},
+		toggleUnLike: function() {
+			// 如果再次被选中
+			if(this.unliked) {
+				this.unlikeNumber--;
+			} else {
+				// 如果换选
+				if(this.liked == true) {
+					this.liked = false;
+					this.likeNumber--;
+				}
+				this.unlikeNumber++;
+			}	
+			this.unliked = ! this.unliked;
+		}
+	}
+});
