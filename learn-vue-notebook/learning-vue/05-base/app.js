54e00f60b222423fe59adfa1b2dd1392460b79a1
lyon <326277403@qq.com>
2016-04-13 15:41:23 +0800

    modified vue-06-basic

--- learn-vue-notebook/learning-vue/05-base/app.js
+++ learn-vue-notebook/learning-vue/05-base/app.js
@@ -40,3 +40,45 @@ new Vue({
 		}
 	}
 });
+new Vue({
+	el: "#post1",
+
+	data: {
+		isVisible: true,
+		likeNumber: 0,
+		unlikeNumber: 0,
+		liked: false,
+		unliked: false,
+	},
+
+	methods: {
+		toggleLike: function () {
+			// 如果再次被选中
+			if(this.liked) {
+				this.likeNumber--;
+			} else {
+				// 如果换选
+				if(this.unliked == true) {
+					this.unliked = false;
+					this.unlikeNumber--;
+				}
+				this.likeNumber++;
+			}
+			this.liked = ! this.liked;
+		},
+		toggleUnLike: function() {
+			// 如果再次被选中
+			if(this.unliked) {
+				this.unlikeNumber--;
+			} else {
+				// 如果换选
+				if(this.liked == true) {
+					this.liked = false;
+					this.likeNumber--;
+				}
+				this.unlikeNumber++;
+			}	
+			this.unliked = ! this.unliked;
+		}
+	}
+});
