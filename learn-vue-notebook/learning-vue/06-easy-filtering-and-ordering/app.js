20dd3fa9a5dc3d63bed433368ff54c6c9ea21ce6
lyon <326277403@qq.com>
2016-04-14 03:11:32 +0800

    easy filtering and ordering for vue

--- learn-vue-notebook/learning-vue/06-easy-filtering-and-ordering/app.js
+++ learn-vue-notebook/learning-vue/06-easy-filtering-and-ordering/app.js
@@ -1,1 +1,33 @@
-app.js
+new Vue({
+	el: "#demo",
+	data: {
+		sortKey: '',
+		reverse: -1,
+		search:"",
+		columns: [
+			{cn: "姓名", en: "name"},
+			{cn: "年龄", en: "age"}
+		],
+		peoples: [
+			{name: '翔', age: 20},
+			{name: '宝宝', age: 21},
+			{name: '大叔', age: 22},
+			{name: '大大叔', age: 22},
+			{name: '阿叉', age: 23},
+			{name: '凯子', age: 24}
+		]
+	},
+	methods: {
+		sortBy: function (sortKey) {
+			if (this.sortKey === sortKey) {
+				if(this.reverse === 1)
+					this.reverse = -1;
+				else 
+					this.reverse = 1;
+			} else {
+				this.reverse = -1;
+			}
+			this.sortKey = sortKey;
+		}
+	}
+});
